885d4b116bd93f78de2ddd751b4dca8a05777526
KIM <chk7119@naver.com>
2023-04-19 14:37:25 +0900

    feat : [002-SIGN-UP-MANAGER] Manager(점장)에 대한 SignUp Service 및 Email Verification 구현

---
+++ src/main/java/com/zerobase/reservation/domain/repository/ManagerRepository.java
@@ -0,0 +1,12 @@
+package com.zerobase.reservation.domain.repository;
+
+import com.zerobase.reservation.domain.model.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+    Optional<Manager> findByEmail(String email);
+}
